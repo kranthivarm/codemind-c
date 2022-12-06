48f3e98e269011ff680b047620b3fe8cb2d38311
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2022-12-06 10:57:08 +0530

    #include<stdio.h> int main(){     int n,rem,sum=0,pro=1;     scanf("%d",&n);     while(n){         rem=n%10;         sum=sum+rem;         pro=pro*rem;         n=n/10;     }if(sum==pro) printf("Spy Number");     else printf("Not Spy Number"); }

---
+++ Spy_Number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,rem,sum=0,pro=1;
+    scanf("%d",&n);
+    while(n){
+        rem=n%10;
+        sum=sum+rem;
+        pro=pro*rem;
+        n=n/10;
+    }if(sum==pro) printf("Spy Number");
+    else printf("Not Spy Number");
+}
