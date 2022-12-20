6d27067bb58675719ebef5fc6ceddce4ac3091a0
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2022-12-20 23:08:50 +0530

    #include<stdio.h> int main(){     int n,i,j,x;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=n;j>i;j--)         {             printf(" ");
            }
        for(x=1;x<=(2*i-1);x++)
        {
            printf("%d",i);
        }
    
        printf("
    ");
            
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 int main(){
-    int n,i,j;
+    int n,i,j,x;
     scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=n;j>=1;j--){
-            printf("%d ",j);
-        }printf("
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>i;j--)
+        {
+            printf(" ");
+            
+        }
+    for(x=1;x<=(2*i-1);x++)
+    {
+        printf("%d",i);
+    }
+
+    printf("
 ");
+        
     }
 }
