252397dfe07d7a4e87064140a48925b63cd7ef1e
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2022-12-20 22:04:45 +0530

    #include<stdio.h> int main(){     int n,i;     scanf("%d",&n);     int a[n],count=0,count2=0;     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     for(i=0;i<n;i++){         if(a[i]%2==1) count++;     }     for(i=0;i<n;i++){         if(a[i]%2==1 && i%2==1)         count2++;}         if(count==count2)         printf("True");         else printf("False"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    scanf("%d",&n);
+    int a[n],count=0,count2=0;
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        if(a[i]%2==1) count++;
+    }
+    for(i=0;i<n;i++){
+        if(a[i]%2==1 && i%2==1)
+        count2++;}
+        if(count==count2)
+        printf("True");
+        else printf("False");
+}
