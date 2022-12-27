b03c1388906b607d89f8018b73bbcbac9a865a49
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2022-12-27 11:36:08 +0530

    #include<stdio.h> int main() {     int n,i,a[n],sum=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d",&a[i]);         sum=sum+a[i];     }printf("%d",sum); }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,a[n],sum=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }printf("%d",sum);
+}
