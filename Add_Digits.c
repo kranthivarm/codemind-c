10ef708703a4822b4468d6221cd0dec0196ff5ab
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2023-05-17 12:11:04 +0530

    #include<stdio.h> int main() {     int n,x,rem,sum;     scanf("%d",&n);     x=n;     sum=n;     while(sum/10>0)     {         sum=0;         while(x!=0)         {             rem=x%10;             sum+=rem;             x/=10;         }         x=sum;     }     printf("%d",sum); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-int main(){
-    int n;
+int main()
+{
+    int n,x,rem,sum;
     scanf("%d",&n);
-    int rem,sum=0;
-    while(n>0)
+    x=n;
+    sum=n;
+    while(sum/10>0)
     {
-        rem=n%10;
-        sum=sum+rem;
-        n=n/10;
-        if(n==0 && sum>=10)
+        sum=0;
+        while(x!=0)
         {
-            n=sum;
-            sum=0;
+            rem=x%10;
+            sum+=rem;
+            x/=10;
         }
+        x=sum;
     }
-   
-        printf("%d",sum);
-    }
-    
+    printf("%d",sum);
+}
