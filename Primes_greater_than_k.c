0f82231d24afbe4c80a5bbc918939e92d1f04f0a
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2023-05-18 15:23:39 +0530

    #include<stdio.h> int prime(int x); int main() {     int n,i,j,cnt=0;     scanf("%d",&n);     int a[n],k,x;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(a[i]>=k)         {             x=a[i];             x=prime(x);             if(x==1)cnt++;         }     }     printf("%d",cnt); } int prime(int x) {     int i,cnt=0;     for(i=1;i<=x;i++)     {         if(x%i==0)cnt++;     }     if(cnt==2) return 1;     else return 0; }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int prime(int x);
+int main()
+{
+    int n,i,j,cnt=0;
+    scanf("%d",&n);
+    int a[n],k,x;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>=k)
+        {
+            x=a[i];
+            x=prime(x);
+            if(x==1)cnt++;
+        }
+    }
+    printf("%d",cnt);
+}
+int prime(int x)
+{
+    int i,cnt=0;
+    for(i=1;i<=x;i++)
+    {
+        if(x%i==0)cnt++;
+    }
+    if(cnt==2) return 1;
+    else return 0;
+}
