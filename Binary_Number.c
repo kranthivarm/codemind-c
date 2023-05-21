061be733179415152318d4dda9ac1d968b484158
kranthivarm <119841129+kranthivarm@users.noreply.github.com>
2023-05-21 09:58:33 +0530

    #include<stdio.h> int main() {     int n,i,j,rem,p=1,temp;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)p*=2;     for(i=0;i<p;i++)     {         temp=i;         for(j=n-1;j>=0;j--)         {             rem=temp%2;             a[j]=rem;             temp/=2;         }         for(j=0;j<n;j++)printf("%d",a[j]);         printf(" ");     } }

--- Binary_Number.c
+++ Binary_Number.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,p=1;
+    int n,i,j,rem,p=1,temp;
     scanf("%d",&n);
+    int a[n];
     for(i=0;i<n;i++)p*=2;
-    int a[n],temp,rem;
     for(i=0;i<p;i++)
     {
         temp=i;
         for(j=n-1;j>=0;j--)
         {
             rem=temp%2;
-            temp/=2;
             a[j]=rem;
+            temp/=2;
         }
         for(j=0;j<n;j++)printf("%d",a[j]);
         printf("
